fc3711e9e419251f14925742dffc8051c2d4ec26
manzikki <marko.niinimaki@protonmail.com>
2024-03-04 14:40:58 +0700

    doc

--- trybl.py
+++ trybl.py
@@ -1,14 +1,22 @@
 import asyncio
 from bleak import BleakScanner
 
+mydev = None
+
 async def run():
     devices = await BleakScanner.discover()
-    foundhi = False
     for d in devices:
         if d.name:
             if "Holy" in d.name:
                 print(d)
-                foundhi = True
-                
+                mydev = d
+    if not mydev:
+        print("Did not find the IoT device.")
+    else:
+        #get services
+        print(dir(mydev))
+        #check https://getwavecake.com/blog/reading-your-phones-battery-level-over-bluetooth-ble-with-python-bleak/
+        
+        
 loop = asyncio.get_event_loop()
 loop.run_until_complete(run())
