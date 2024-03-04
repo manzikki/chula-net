1f32a2abe03116dc917851b1412b8fbde960f5f5
manzikki <marko.niinimaki@protonmail.com>
2024-03-04 14:59:36 +0700

    read services

--- trybl.py
+++ trybl.py
@@ -1,9 +1,9 @@
 import asyncio
-from bleak import BleakScanner
-
-mydev = None
+from bleak import BleakScanner, BleakClient
 
 async def run():
+    mydev = None
+    myaddr = ""
     devices = await BleakScanner.discover()
     for d in devices:
         if d.name:
@@ -14,9 +14,21 @@ async def run():
         print("Did not find the IoT device.")
     else:
         #get services
-        print(dir(mydev))
+        #print(dir(mydev)) #address, details etc
         #check https://getwavecake.com/blog/reading-your-phones-battery-level-over-bluetooth-ble-with-python-bleak/
-        
-        
+        print(mydev.address)
+        myaddr = mydev.address
+    if not myaddr:
+        print("Could not get the address of the device.")
+    else:
+        async with BleakClient(myaddr) as client:
+            svcs = client.services
+            print("Services:")
+            for service in svcs:
+                print(service)
+#00001800-0000-1000-8000-00805f9b34fb (Handle: 1): Generic Access Profile
+#00001801-0000-1000-8000-00805f9b34fb (Handle: 10): Generic Attribute Profile
+#6e400001-b5a3-f393-e0a9-e50e24dcca9e (Handle: 11): Nordic UART Service
+
 loop = asyncio.get_event_loop()
 loop.run_until_complete(run())
